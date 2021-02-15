e885b9f7bc901ee566c2bcd2a68ff0fce3896b7b
Aleksei Draznin <aleksei.draznin@t-systems.com>
2021-02-15 23:44:30 +0300

    Add filter for path header

--- src/main/java/ru/tsystems/devschool/DevSchoolApplication.java
+++ src/main/java/ru/tsystems/devschool/DevSchoolApplication.java
@@ -1,7 +1,12 @@
 package ru.tsystems.devschool;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 
 /**
  * Service entry point
@@ -10,5 +15,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevSchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevSchoolApplication.class, args);
+    }
+
+    @Autowired
+    private ServerProperties serverProperties;
+
+    @Bean
+    public FilterRegistrationBean tvppFaultReportLoggingFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setOrder(Ordered.LOWEST_PRECEDENCE);
+        registration.setFilter(new HeaderFilter(serverProperties.getPort().toString()));
+        return registration;
     }
 }
---
+++ src/main/java/ru/tsystems/devschool/HeaderFilter.java
@@ -0,0 +1,33 @@
+package ru.tsystems.devschool;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class HeaderFilter extends OncePerRequestFilter {
+
+    private String hostAddress;
+    public HeaderFilter(String port) {
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostAddress = ip.getHostAddress() + ":" + port;
+        } catch (UnknownHostException e) {
+            hostAddress = null;
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Full-Path", hostAddress);
+        filterChain.doFilter(request, response);
+    }
+}
